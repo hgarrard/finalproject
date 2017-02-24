6b296e92e8d728325a3ac783818dd40f68c47de1
hgarrard <hgarrard78@gmail.com>
2017-02-24 03:48:54 +0000

    catalog_cart

--- test/models/product_test.rb
+++ test/models/product_test.rb
@@ -1,4 +1,9 @@
-require 'test_helper'
-class ProductTest < ActiveSupport::TestCase
-fixtures :products
+test "product is not valid without a unique title - i18n" do
+product = Product.new(title: products(:ruby).title,
+description: "yyy",
+price: 1,
+image_url: "fred.gif")
+assert product.invalid?
+assert_equal [I18n.translate('errors.messages.taken')],
+product.errors[:title]
 end
