52078643577506a70e8c70ea3347cfd6e80ff1d9
Heather Garrard <hgarrard78@gmail.com>
2017-03-15 18:21:18 +0000

    finishing touches

--- app/controllers/static_pages_controller.rb
+++ app/controllers/static_pages_controller.rb
@@ -1,5 +1,11 @@
-skip_before_action :authorize
-    def home
-    end
- 
+class StaticPagesController < ApplicationController
+#skip_before_action :authorize
+  def home
+  end
 
+  def help
+  end
+
+  def ads
+  end
+end
