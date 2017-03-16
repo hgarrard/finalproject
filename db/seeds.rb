07d4cc230080c6cb86b59d085b0238ed68350f64
Heather Garrard <hgarrard78@gmail.com>
2017-03-16 17:01:40 +0000

    final project modifications

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,47 +1,36 @@
 Product.delete_all
-Product.create(title: 'Rails, Angular, Postgres, and Bootstrap',
+Product.create(title: '2014 Mark West Pinot Noir',
   description:
     %{<p>
-      <em>Powerful, Effective, and Efficient Full-Stack Web Development</em>
-      As a Rails developer, you care about user experience and performance,
-      but you also want simple and maintainable code. Achieve all that by
-      embracing the full stack of web development, from styling with
-      Bootstrap, building an interactive user interface with AngularJS, to
-      storing data quickly and reliably in PostgreSQL. Take a holistic view of
-      full-stack development to create usable, high-performing applications,
-      and learn to use these technologies effectively in a Ruby on Rails
-      environment.
+      <em>2014 Mark West Pinot Noir</em> 
+     A pleasing mix of black cherry, cola, strawberry, plum, and soft tannins.
       </p>},
-  image_url: 'dcbang.jpg',    
-  price: 45.00)
-# . . .
-Product.create(title: 'Seven Mobile Apps in Seven Weeks',
-  description:
-    %{<p>
-      <em>Native Apps, Multiple Platforms</em>
-      Answer the question “Can we build this for ALL the devices?” with a
-      resounding YES. This book will help you get there with a real-world
-      introduction to seven platforms, whether you’re new to mobile or an
-      experienced developer needing to expand your options. Plus, you’ll find
-      out which cross-platform solution makes the most sense for your needs.
-      </p>},
-  image_url: '7apps.jpg',
-  price: 26.00)
-# . . .
+  image_url: 'markwest.jpg',
+  price: 8.99)
 
-Product.create(title: 'Ruby Performance Optimization',
+Product.create(title: '2014 Apothic Red',
   description:
     %{<p>
-      <em>Why Ruby Is Slow, and How to Fix It</em> 
-      You don’t have to accept slow Ruby or Rails performance. In this
-      comprehensive guide to Ruby optimization, you’ll learn how to write
-      faster Ruby code—but that’s just the beginning. See exactly what makes
-      Ruby and Rails code slow, and how to fix it. Alex Dymo will guide you
-      through perils of memory and CPU optimization, profiling, measuring,
-      performance testing, garbage collection, and tuning. You’ll find that
-      all those “hard” things aren’t so difficult after all, and your code
-      will run orders of magnitude faster.
+      <em>2014 Apothic Red</em> 
+     A bold blend of primarily Zinfandel, Syrah, Merlot and Cabernet Sauvignon, Apothic Red leaves a lasting impression of flavor and texture on the palate. Zinfandel offers bold dark fruit flavors and spicy notes, complemented by the soft mouthfeel of Merlot. The Syrah adds layers of smooth blueberry, while the Cabernet Sauvignon delivers ripe fruit flavors and a firm structure. This decadent blend is framed by hints of black cherry, vanilla, and mocha.
       </p>},
-  image_url: 'adrpo.jpg',
-  price: 46.00)
-
+  image_url: 'apothic.jpg',
+  price: 7.79)
+  
+  Product.create(title: '2015 Chicago Cubs Club Series Red Blend',
+  description:
+    %{<p>
+      <em>2015 Chicago Cubs Club Series Red Blend</em> 
+     There are times when the North side of Chicago seems so cold that nothing could survive except for the ivy that grows on the outfield wall at Wrigley Field and the fans that watch the Cubs play there. You can stay warm with this full-bodied, well-balanced Cabernet, layered with aromas of ultra-ripe black fruit. Thriving is so much better than surviving.
+      </p>},
+  image_url: '947431L.jpg',
+  price: 16.99)
+  
+    Product.create(title: '2015 The Prisoner',
+  description:
+    %{<p>
+      <em>2015 The Prisoner</em> 
+     The Prisoner was created and inspired by the drinkable “mixed blacks” first made by the Italian immigrants who originally settled in the Napa Valley. Features enticing aromas of Bing cherry, dark chocolate, clove, and roasted fig. Persistent flavors of ripe raspberry, boysenberry, pomegranate, and vanilla linger harmoniously, for a smooth and luscious finish.
+      </p>},
+  image_url: 'prisoner.jpg',
+  price: 37.99)
